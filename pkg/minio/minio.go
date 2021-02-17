8f2f7827a2c79e9e53802c7d4dcf0e67a1f0cc7d
jebram <ali.blaster7@gmail.com>
2021-02-18 00:10:30 +0500

    created mino settings and cred constants.

---
+++ pkg/minio/minio.go
@@ -0,0 +1,9 @@
+package minio
+
+//Mino settings and credentials
+const (
+	Endpoint        = "play.min.io"
+	AccessKeyID     = "Q3AM3UQ867SPQQA43P2F"
+	SecretAccessKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	UseSSL          = true
+)
