8356d3abd9f0eb8c20160f23540d267bd2b832ec
jebram <ali.blaster7@gmail.com>
2021-02-18 00:12:10 +0500

    created function to get minio client.

--- pkg/minio/minio.go
+++ pkg/minio/minio.go
@@ -1,5 +1,10 @@
 package minio
 
+import (
+	"github.com/minio/minio-go"
+	"github.com/minio/minio-go/pkg/credentials"
+)
+
 //Mino settings and credentials
 const (
 	Endpoint        = "play.min.io"
@@ -7,3 +12,14 @@ const (
 	SecretAccessKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
 	UseSSL          = true
 )
+
+//Get mini client
+func getMinioInstance() (*minio.Client, error) {
+
+	Client, err := minio.New(Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(AccessKeyID, SecretAccessKey, ""),
+		Secure: UseSSL,
+	})
+
+	return Client, err
+}
